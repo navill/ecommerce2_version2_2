0cf24c0e66fdd203148d1681a805eddc237cbc37
jihoon <blue_jihoon@naver.com>
2019-12-26 01:14:44 +0900

    first commit

---
+++ src/products/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(null=True,blank=True)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    activate = models.BooleanField(default=True)
+    #slug
+    #inventory
+
+    def __str__(self):
+        return self.title
